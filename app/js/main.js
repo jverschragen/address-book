be4b3eef9ebc910088e679524ba3a68931b64fc3
Jelle Verschragen <jelle@hotmail.com>
2018-10-08 10:00:26 +0200

    Working on search function for contacts

--- app/js/main.js
+++ app/js/main.js
@@ -7,22 +7,39 @@ function append(parent, el) {
 }
 
 const ul = document.getElementById('contacts');
-const url = 'https://randomuser.me/api/?results=10';
+const url = 'https://randomuser.me/api/?nat=us,dk,fr,gb&results=10';
+
+const contacts = [];
+
 fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let contacts = data.results;
-        return contacts.map(function(contact) {
-            let li = createNode('li'),
-                img = createNode('img'),
-                span = createNode('span');
-            img.src = contact.picture.medium;
-            span.innerHTML = `${contact.name.first} ${contact.name.last}`;
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-        })
-    })
+    .then(blob => blob.json())
+    .then(data => contacts.push(...data))
     .catch(function(error) {
         console.log(error);
     });
+
+function showContacts() {
+    // sort by firstname (alphabetically)
+    contacts.sort(function (a, b) {
+        if (a.name.first < b.name.first) return -1;
+        if (a.name.first > b.name.first) return 1;
+    });
+    // return a list with contacts
+    return contacts.map(function (contact) {
+        let li = createNode('li'),
+            img = createNode('img'),
+            spanName = createNode('span.name');
+        spanPhone = createNode('span.phoneNumber');
+        img.src = contact.picture.medium;
+
+        spanName.innerHTML = `${contact.name.first} ${contact.name.last}`;
+        spanPhone.innerHTML = `${contact.phone}`;
+        append(li, img);
+        append(li, spanName);
+        append(li, spanPhone);
+        append(ul, li);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", showContacts);
+
